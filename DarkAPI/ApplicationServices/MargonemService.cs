a4d94ae2d2d8a02d806f58054365c8fa73bd40d3
Sebastian00P <w64182@student.wsiz.edu.pl>
2023-11-19 22:52:36 +0100

    INIT

---
+++ DarkAPI/ApplicationServices/MargonemService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkAPI.ApplicationServices
+{
+    public class MargonemService : IMargonemService
+    {
+        public string FilePath = "";
+        private readonly IConfiguration _services;
+        public MargonemService(IConfiguration services)
+        {
+            _services = services;
+            FilePath = _services.GetValue<string>("AddressesFilePath");
+        }
+        public List<string> ReadAddressesFromFile()
+        {
+            string filePath = FilePath;
+            List<string> addresses = new List<string>();
+
+            try
+            {            
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        addresses.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Wystąpił błąd podczas czytania pliku: " + e.Message);
+            }
+
+            return addresses;
+        }
+    }
+}
---
+++ DarkAPI/Controllers/MargonemController.cs
@@ -0,0 +1,34 @@
+﻿using DarkAPI.ApplicationServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DarkAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MargonemController : ControllerBase
+    {
+        private readonly IMargonemService _margonemService;
+        public MargonemController(IMargonemService margonemService)
+        {
+            _margonemService = margonemService;
+        }
+        [HttpGet]
+        public bool GetUserToken()
+        {
+            var userIpAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            var addresses = _margonemService.ReadAddressesFromFile();
+            if (addresses.Contains(userIpAddress))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
